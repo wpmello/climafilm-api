3ee533dba1bb89774bf5e46e1c37f2344a1ff887
wagner <wagnermellow2k@gmail.com>
2023-08-11 14:24:06 -0300

    master: added api documentation with swagger

--- src/main/java/com/example/project/api/controller/MovieController.java
+++ src/main/java/com/example/project/api/controller/MovieController.java
@@ -5,6 +5,9 @@ import com.example.project.api.model.dto.PosterDTO;
 import com.example.project.api.model.themovie.BodyMovie;
 import com.example.project.api.service.MovieService;
 import com.example.project.api.service.exceptions.MovieNotFoundException;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -20,16 +23,32 @@ public class MovieController {
 
     private MovieService movieService;
 
+    @Operation(summary = "Retorna temperatura atual dado uma cidade.", method = "GET")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Retorna temperatura atual com sucesso."),
+            @ApiResponse(responseCode = "404", description = "Temperatura atual não encontrada."),
+            @ApiResponse(responseCode = "500", description = "Erro ao tentar retornar temperatura atual.")
+    })
     @GetMapping("/{city}")
-    public Long getCity(@PathVariable String city) {
+    public Long getTempCity(@PathVariable String city) {
         return this.movieService.getTempCity(city);
     }
 
+    @Operation(summary = "Retorna os filmes que estão em cartaz no momento.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Retorna os filmes que estão em cartaz no momento com sucesso."),
+            @ApiResponse(responseCode = "500", description = "Erro ao tentar retornar os filmes que estão em cartaz no momento.")
+    })
     @GetMapping("/on-playing")
     public PosterDTO getMovieOnPlayingNow() {
         return this.movieService.getMovieOnPlayingNow();
     }
 
+    @Operation(summary = "Dado uma cidade, retorna os filmes que estão em cartaz no momento filtrados por gêneros.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Retorna os filmes que estão em cartaz no momento, filtrados por gêneros, com sucesso."),
+            @ApiResponse(responseCode = "500", description = "Erro ao tentar retornar os filmes que estão em cartaz no momento, filtrados por gênero.")
+    })
     @GetMapping("/on-playing/{city}")
     public List<BodyMovieDTO> getMoviePerCity(@PathVariable String city) {
         return this.movieService.getMovieOnPlayingNowPerCity(city);
