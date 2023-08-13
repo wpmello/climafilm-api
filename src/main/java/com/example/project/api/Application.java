64beff2c1808776744381a928f699728c757985d
wagner <wagnermellow2k@gmail.com>
2023-08-12 21:15:38 -0300

    master: update api documentation

--- src/main/java/com/example/project/api/Application.java
+++ src/main/java/com/example/project/api/Application.java
@@ -1,12 +1,9 @@
 package com.example.project.api;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@OpenAPIDefinition(info = @Info(title = "ClimaFilm Api", version = "1", description = "Api desenvolvida para filtrar filmes em cartaz com base na temperatura atual de determinado estado ou país."))
 public class Application {
 
     public static void main(String[] args) {
--- src/main/java/com/example/project/api/service/config/WebConfig.java
+++ src/main/java/com/example/project/api/service/config/WebConfig.java
@@ -1,5 +1,9 @@
 package com.example.project.api.service.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,6 +12,8 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.util.List;
 
 @Configuration
 @EnableWebMvc
@@ -19,6 +25,18 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     private String allowedMethods;
     @Value("${cors.max-age}")
     private long maxAge;
+    @Value("${documentation.api.title}")
+    private String apiTitle;
+    @Value("${documentation.api.description}")
+    private String apiDescription;
+    @Value("${documentation.api.contact-name}")
+    private String contactName;
+    @Value("${documentation.api.contact-email}")
+    private String contactEmail;
+    @Value("${documentation.api.contact-github}")
+    private String contactGithub;
+
+
 
     @Bean
     public RestTemplate restTemplate() {
@@ -41,5 +59,16 @@ public class WebConfig extends WebMvcConfigurerAdapter {
                         .maxAge(maxAge);
             }
         };
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .servers(List.of(new Server().url(allowedOrigins)))
+                .info(new Info()
+                        .title(apiTitle)
+                        .description(apiDescription)
+                        .contact(new Contact().name(contactName).email(contactEmail).url(contactGithub))
+                        .version("1.0.0"));
     }
 }
