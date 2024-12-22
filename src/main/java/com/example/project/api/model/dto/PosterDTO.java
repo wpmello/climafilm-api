c251fd10dd1faa668a7f31701db3297738a5dadc
wagne <wagnermellow2k@gmail.com>
2024-12-22 20:18:23 -0300

    remove mappers and dtos

--- src/main/java/com/example/project/api/model/dto/PosterDTO.java
+++
@@ -1,8 +0,0 @@
-package com.example.project.api.model.dto;
-
-import lombok.Builder;
-
-import java.util.List;
-
-@Builder
-public record PosterDTO(List<BodyMovieDTO> results, Integer total_results) { }
